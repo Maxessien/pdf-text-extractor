d78167cf0357f703f6f072c7781edb5532a4dccd
Maxessien <essienmax484@gmail.com>
2026-01-11 19:17:39 +0100

    Fixed app.py syntax error

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def serve_assets(id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use Render's port, fallback to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)s
+    app.run(host="0.0.0.0", port=port, debug=True)
